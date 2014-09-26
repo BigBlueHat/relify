3c44f6254ad550c98b6f8ba7cb7641094ad5c22c
BigBlueHat <byoung@bigbluehat.com>
2014-09-26 13:38:55 -0400

    General ledger log-style MapReduce-age

---
+++ couchapp/views/links/map.js
@@ -0,0 +1,8 @@
+function(doc) {
+  if ('links' in doc) {
+    for (var i = 0; i < doc.links.length; i++) {
+      emit([doc.method, doc.links[i].rel, doc.links[i].href],
+        (doc.method == 'LINK' ? 1 : -1));
+    }
+  }
+}
